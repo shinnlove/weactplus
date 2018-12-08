059113cf4def0fdff3e5b3b9fe986b53fb405f54
chensheng.zcs <chensheng.zcs@alipay.com>
2018-12-08 12:15:32 +0800

    修改数据库配置

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '10.8.52.100',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'ad_supplier',
+    'database'        => 'weact',
     // 用户名
-    'username'        => 'ad_supplier_r',
+    'username'        => 'root',
     // 密码
-    'password'        => 'aVObkvbUCrQO0oX_UaNpxReJr16FQ1QV',
+    'password'        => '',
     // 端口
     'hostport'        => '3883',
     // 连接dsn
