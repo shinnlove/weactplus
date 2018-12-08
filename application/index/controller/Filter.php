d5bcd12bfae3bb540cb7b1c3b1685844f2357a61
chensheng.zcs <chensheng.zcs@alipay.com>
2018-12-08 14:04:47 +0800

    开启调试模式，增加新的控制器

---
+++ application/index/controller/Filter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+
+class Filter extends Controller {
+
+    public function sso() {
+
+
+
+        return $this->fetch();
+    }
+
+}
+
+?>
+
